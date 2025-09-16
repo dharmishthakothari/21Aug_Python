03ed5b3d57fa0e56eb83ae9f7dc40b6e90c273f7
dharmishthakothari <dharmishtha.only@gmail.com>
2025-09-16 09:59:04 +0530

    Add files via upload

---
+++ 21_Aug_Python/break_stmt.py
@@ -0,0 +1,11 @@
+# for i in range(10):
+#     print(i)
+#     if i==4:
+#         break
+# print("END")
+i=0
+while i<=10:
+    print(i)
+    if i==4:
+        break
+    i+=1
---
+++ 21_Aug_Python/string_example.py
@@ -0,0 +1,11 @@
+c_name = "Tops Technologies pvt ltd"
+s=c_name.upper()
+print(s)
+s=c_name.lower()
+print(s)
+s=c_name.title()
+print("Title ",s)
+s=c_name.capitalize()
+print("capitlized ",s)
+
+
